b6fb918c54eac9d3d11d2b962742a9a76ac8657a
cdzm5211314 <123309778@qq.com>
2017-07-27 10:39:07 +0800

    使用Struts2完成文件上传操作

--- src/cn/itcast/web/action/LinkManAction.java
+++ src/cn/itcast/web/action/LinkManAction.java
@@ -1,7 +1,10 @@
 package cn.itcast.web.action;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.struts2.ServletActionContext;
 
 import com.opensymphony.xwork2.ActionSupport;
@@ -31,6 +34,25 @@ public class LinkManAction extends ActionSupport implements ModelDriven<LinkMan>
 		return linkMan;
 	}
 	
+	//1.上传文件:变量的名称是表单里面文件上传项的name值:如upload
+	private File upload;
+	//2.上传文件名称:变量的名称是表单里面文件上传项name值+FileName
+	private String uploadFileName;
+	
+	//生成get/set方法
+	public File getUpload() {
+		return upload;
+	}
+	public void setUpload(File upload) {
+		this.upload = upload;
+	}
+	public String getUploadFileName() {
+		return uploadFileName;
+	}
+	public void setUploadFileName(String uploadFileName) {
+		this.uploadFileName = uploadFileName;
+	}
+	
 	//到添加联系人页面
 	public String toAddPage(){
 		//查询所有的数据显示在页面的下拉列表中
@@ -42,10 +64,15 @@ public class LinkManAction extends ActionSupport implements ModelDriven<LinkMan>
 	}
 	
 	//添加联系人信息
-	public String  addLinkMan(){
-		
+	public String  addLinkMan() throws Exception{
+		//判断是否需要上传文件
+		if (upload != null) {
+			//需要上传文件--->在服务器上的指定位置创建文件
+			File serverFile = new File("F:\\我的下载"+"\\"+uploadFileName);
+			//把上传文件内容复制到服务器创建的文件里面
+			FileUtils.copyFile(upload, serverFile);
+		}
 		linkManService.addLinkMan(linkMan);
-		
 		return "addLinkManSUCCESS";
 	}
 
